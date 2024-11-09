d5d691be8694cf87ea90b8613fbc8661d76025b4
Danial <sdnoaein@gmail.com>
2024-11-09 12:59:54 +0330

    update funds tabs

--- src/views/components/nav/Nav.tsx
+++ src/views/components/nav/Nav.tsx
@@ -1,31 +1,73 @@
 "use client";
 
+import {
+  IconCaretDown,
+  IconCashBanknote,
+  IconInbox,
+  IconMenu,
+} from "@tabler/icons-react";
 import Image from "next/image";
 
 const Nav = () => {
   return (
-    <div className='fixed top-0 w-full flex justify-center z-50 mt-8'>
-      <nav className='container bg-primary-500 rounded-full p-4 shadow-lg flex justify-between items-center bg-pattern'>
-        <div className='container mx-auto flex justify-between items-center'>
-          <a className='text-white font-bold text-lg mr-4'>
-            <Image
-              src={"/images/nav-logo.png"}
-              width={90}
-              height={28}
-              alt='Nav Logo'
-            />
-          </a>
-          <div className='flex-1 flex items-center align-center justify-center gap-8'>
-            <a className='text-white hover:text-blue-200'>سرمایه گذاری</a>
-            <a className='text-white hover:text-blue-200'>معاملات</a>
-            <a className='text-white hover:text-blue-200'>لیزینگ</a>
-            <a className='text-white hover:text-blue-200'>ارتباط با ما</a>
+    <div className='fixed top-0 w-full flex flex-col items-center justify-center z-50 mt-8'>
+      <div className='container '>
+        <nav className=' bg-primary-500 rounded-[3rem] p-4 shadow-lg flex justify-between items-center bg-pattern'>
+          <div className='container mx-auto flex justify-between items-center'>
+            <IconMenu className='text-white hidden' />
+            <a className='text-white font-bold text-lg mr-4'>
+              <Image
+                src={"/images/nav-logo.png"}
+                width={90}
+                height={28}
+                alt='Nav Logo'
+              />
+            </a>
+            <div className='flex-1 flex items-center align-center justify-center gap-8'>
+              <a className='text-white hover:text-blue-200'>سرمایه گذاری</a>
+              <a className='text-white hover:text-blue-200 flex align-center justify-center'>
+                <IconCaretDown />
+                معاملات
+              </a>
+              <a className='text-white hover:text-blue-200 flex align-center justify-center'>
+                <IconCaretDown />
+                لیزینگ
+              </a>
+              <a className='text-white hover:text-blue-200'>ارتباط با ما</a>
+            </div>
+            <a className='text-white hover:text-blue-200 rounded-full border-2 px-4 py-2'>
+              ورود | ثبت نام
+            </a>
           </div>
-          <a className='text-white hover:text-blue-200 rounded-full border-2 px-4 py-2'>
-            ورود | ثبت نام
+        </nav>
+        <div className='hidden z-0 container text-white bg-primary-500 mt-[-40px] p-4 shadow-lg flex rounded-b-[2.5rem] justify-center items-center bg-pattern pt-10 gap-4'>
+          <a
+            href='#'
+            className='flex border rounded-xl px-4 py-2 gap-2 border-dashed'
+          >
+            <IconInbox />
+            <div>
+              <div className='font-bold'>صندوق های سرمایه‌گذاری</div>
+              <div className=' text-sm text-gray-300'>
+                صندوق های سرمایه‌گذاری
+              </div>
+            </div>
+          </a>
+
+          <a
+            href='#'
+            className='flex border rounded-xl px-4 py-2 gap-2 border-dashed'
+          >
+            <IconCashBanknote />
+            <div>
+              <div className='font-bold'>صندوق های سرمایه‌گذاری</div>
+              <div className=' text-sm text-gray-300'>
+                صندوق های سرمایه‌گذاری
+              </div>
+            </div>
           </a>
         </div>
-      </nav>
+      </div>
     </div>
   );
 };
--- src/views/pages/home/Tabs.tsx
+++ src/views/pages/home/Tabs.tsx
@@ -1,19 +1,21 @@
+import { IconCaretUp, IconInfoCircle, IconPencil } from "@tabler/icons-react";
+import Image from "next/image";
 import { useState } from "react";
 
 const tabsData = [
   {
-    label: "صندوق بهگزین",
+    title: "صندوق بهگزین",
     content:
       "Ut irure mollit nulla eiusmod excepteur laboris elit sit anim magna tempor excepteur labore nulla.",
   },
   {
-    label: "صندوق پتروپاداش",
+    title: "صندوق پتروپاداش",
     content:
       "Fugiat dolor et quis in incididunt aute. Ullamco voluptate consectetur dolor officia sunt est dolor sint.",
   },
 
   {
-    label: "صندوق پاداش",
+    title: "صندوق پاداش",
     content:
       "Fugiat dolor et quis in incididunt aute. Ullamco voluptate consectetur dolor officia sunt est dolor sint.",
   },
@@ -22,6 +24,122 @@ const tabsData = [
 const Tabs = () => {
   const [activeTabIndex, setActiveTabIndex] = useState(0);
 
+  const content = (activeTabIndex: number) => {
+    return (
+      <div className={`relative mx-auto bg-[#FAFBFF] overflow-hidden`}>
+        <div className={`flex items-center`}>
+          <div className='flex-1 relative'>
+            <div className='relative z-[1] flex  justify-center items-center flex-col text-white'>
+              <Image
+                src={"/images/fund-section-item.png"}
+                alt={"section"}
+                width={400}
+                height={400}
+              />
+              <div className='px-8 mt-[-32px]'>
+                <div className='font-bold text-2xl'>
+                  {tabsData[activeTabIndex].title}
+                </div>
+                <div className='text-sm mb-4'>
+                  صندوق سرمایه‌گذاری اهرمی پاداش (واحدهای ممتاز)
+                </div>
+                <div>
+                  با سرمایه‌گذاری در صندوق اهرمی کاریزما می‌تونی بیشتر از دارایی
+                  خودت در بورس سرمایه‌گذاری کنی و به صورت اهرمی در بورس بازدهی
+                  کسب کنی. صندوق اهرمی کاریزما نخستین و بزرگترین صندوق از نوع
+                  اهرمی در بازار سرمایه است.
+                </div>
+                <div className='text-left mt-4'>اطلاعات بیشتر</div>
+              </div>
+            </div>
+            <div className='absolute bottom-[-50px] z-0'>
+              <Image
+                src={"/images/cards-pocket-bg.png"}
+                alt={"section"}
+                width={800}
+                height={400}
+              />
+            </div>
+          </div>
+          <div className='flex-1 mx-8 pb-16'>
+            <div className='text-sm shadow w-20 rounded-full text-center mt-12 mb-6'>
+              تحلیل
+            </div>
+            <div className='mb-6 text-4xl'>شرکت سبدگردان پاداش</div>
+            <div className='text-justify'>
+              کارگزاری پاداش از جمله کارگزاری‌هایی است که فرآیند ثبت نام و احراز
+              هویت را به صورت غیرحضوری انجام می‌دهد. افتتاح حساب شما در کارگزاری
+              پاداش کمتر از 5 دقیقه زمان نیاز دارد. شرکت کارگزاری پاداش با
+              مدیریت حرفه ای و بهره گیری از نیروهای متخصص و باتجربه سعی در ارائه
+              خدمات سرمایه گذاری در بازار سرمایه اعم از بورس اوراق بهادار، بورس
+              کالا، انرژی و ابزار مشتقه، د
+            </div>
+            <div className='grid grid-cols-2 gap-4 mt-8'>
+              <div className='border-2 rounded-xl p-4 border-dashed text-center flex flex-col gap-1 '>
+                <div className='flex justify-center gap-1'>
+                  قیمت پایانی
+                  <IconInfoCircle className='text-gray-400' />
+                </div>
+                <div className='flex gap-1 justify-center items-center'>
+                  <span className='text-red-500'></span>
+                  <span className='text-green-500 flex text-xl'>
+                    <IconCaretUp /> (2.37%)
+                  </span>
+                  <span className='font-bold text-xl'>43,991</span>
+                  <span className='text-xs'>ریال</span>
+                </div>
+              </div>
+
+              <div className='border-2 rounded-xl p-4 border-dashed text-center flex flex-col gap-1 '>
+                <div className='flex justify-center gap-1'>
+                  قیمت پایانی
+                  <IconInfoCircle className='text-gray-400' />
+                </div>
+                <div className='flex gap-1 justify-center items-center'>
+                  <span className='text-red-500'></span>
+                  <span className='text-green-500 flex text-xl'>
+                    <IconCaretUp /> (2.37%)
+                  </span>
+                  <span className='font-bold text-xl'>43,991</span>
+                  <span className='text-xs'>ریال</span>
+                </div>
+              </div>
+
+              <div className='border-2 rounded-xl p-4 border-dashed text-center flex flex-col gap-1 '>
+                <div className='flex justify-center gap-1'>
+                  قیمت پایانی
+                  <IconInfoCircle className='text-gray-400' />
+                </div>
+                <div className='flex gap-1 justify-center items-center'>
+                  <span className='text-red-500'></span>
+                  <span className='text-green-500 flex text-xl'>
+                    <IconCaretUp /> (2.37%)
+                  </span>
+                  <span className='font-bold text-xl'>43,991</span>
+                  <span className='text-xs'>ریال</span>
+                </div>
+              </div>
+
+              <div className='border-2 rounded-xl p-4 border-dashed text-center flex flex-col gap-1 '>
+                <div className='flex justify-center gap-1'>
+                  قیمت پایانی
+                  <IconInfoCircle className='text-gray-400' />
+                </div>
+                <div className='flex gap-1 justify-center items-center'>
+                  <span className='text-red-500'></span>
+                  <span className='text-green-500 flex text-xl'>
+                    <IconCaretUp /> (2.37%)
+                  </span>
+                  <span className='font-bold text-xl'>43,991</span>
+                  <span className='text-xs'>ریال</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
   return (
     <div className='container mx-auto my-16'>
       <div className='flex'>
@@ -34,14 +152,14 @@ const Tabs = () => {
               // Change the active tab on click.
               onClick={() => setActiveTabIndex(idx)}
             >
-              {tab.label}
+              {tab.title}
             </button>
           );
         })}
       </div>
       {/* Show active tab content. */}
-      <div className='py-4 border bg-[#FAFBFF]'>
-        <p>{tabsData[activeTabIndex].content}</p>
+      <div className='border bg-[#FAFBFF]'>
+        <div>{content(activeTabIndex)}</div>
       </div>
     </div>
   );
