76261b2d08d6b097c1a28bc709286471c74c6083
Danial <sdnoaein@gmail.com>
2024-11-10 21:08:56 +0330

    update main menu

--- src/views/components/nav/Nav.tsx
+++ src/views/components/nav/Nav.tsx
@@ -2,7 +2,8 @@
 
 import {
   IconCaretDown,
-  IconCashBanknote,
+  IconCaretUp,
+  IconChevronLeft,
   IconInbox,
   IconMenu,
 } from "@tabler/icons-react";
@@ -12,7 +13,7 @@ const Nav = () => {
   return (
     <div className='fixed top-0 w-full flex flex-col items-center justify-center z-50 mt-8'>
       <div className='container '>
-        <nav className=' bg-primary-500 rounded-[3rem] p-4 shadow-lg flex justify-between items-center bg-pattern'>
+        <nav className='bg-primary-500 rounded-[3rem] p-4 shadow-lg flex justify-between items-center bg-pattern relative z-[1]'>
           <div className='container mx-auto flex justify-between items-center'>
             <IconMenu className='text-white hidden' />
             <a className='text-white font-bold text-lg mr-4'>
@@ -23,50 +24,65 @@ const Nav = () => {
                 alt='Nav Logo'
               />
             </a>
-            <div className='flex-1 flex items-center align-center justify-center gap-8'>
-              <a className='text-white hover:text-blue-200'>سرمایه گذاری</a>
-              <a className='text-white hover:text-blue-200 flex align-center justify-center'>
-                <IconCaretDown />
-                معاملات
-              </a>
-              <a className='text-white hover:text-blue-200 flex align-center justify-center'>
-                <IconCaretDown />
-                لیزینگ
-              </a>
-              <a className='text-white hover:text-blue-200'>ارتباط با ما</a>
+            <div className='flex-1 '>
+              <ul className='flex items-center align-center justify-center gap-8'>
+                <li>
+                  <a
+                    href='#'
+                    className='text-white hover:text-blue-200'
+                  >
+                    سرمایه گذاری
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href='#'
+                    className='text-white hover:text-blue-200 flex align-center justify-center'
+                  >
+                    <IconCaretDown />
+                    معاملات
+                  </a>
+                </li>
+                <li className='group py-4'>
+                  <a
+                    href='#'
+                    className='text-white hover:text-blue-200 flex align-center justify-center'
+                  >
+                    <IconCaretDown className='group-hover:hidden block' />
+                    <IconCaretUp className='group-hover:block hidden' />
+                    لیزینگ
+                  </a>
+                  <div className='group-hover:flex hidden rounded-b-[2.5rem] absolute gap-8 top-[42px] z-[-1] pt-[48px] pb-[24px] right-0 w-full bg-pattern bg-primary-500 items-center justify-center'>
+                    <a
+                      href='#'
+                      className='flex border rounded-xl px-4 py-2 gap-2 border-dashed transition hover:shadow-xl text-white'
+                    >
+                      <IconInbox />
+                      <div>
+                        <div className='font-bold'>صندوق های سرمایه‌گذاری</div>
+                        <div className=' text-sm'>صندوق های سرمایه‌گذاری</div>
+                      </div>
+                      <div className='flex items-center'>
+                        <IconChevronLeft className='group-hover:opacity-100 opacity-0 transition' />
+                      </div>
+                    </a>
+                  </div>
+                </li>
+                <li>
+                  <a
+                    href='#'
+                    className='text-white hover:text-blue-200'
+                  >
+                    ارتباط با ما
+                  </a>
+                </li>
+              </ul>
             </div>
             <a className='text-white hover:text-blue-200 rounded-full border-2 px-4 py-2'>
               ورود | ثبت نام
             </a>
           </div>
         </nav>
-        <div className='hidden z-0 container text-white bg-primary-500 mt-[-40px] p-4 shadow-lg flex rounded-b-[2.5rem] justify-center items-center bg-pattern pt-10 gap-4'>
-          <a
-            href='#'
-            className='flex border rounded-xl px-4 py-2 gap-2 border-dashed'
-          >
-            <IconInbox />
-            <div>
-              <div className='font-bold'>صندوق های سرمایه‌گذاری</div>
-              <div className=' text-sm text-gray-300'>
-                صندوق های سرمایه‌گذاری
-              </div>
-            </div>
-          </a>
-
-          <a
-            href='#'
-            className='flex border rounded-xl px-4 py-2 gap-2 border-dashed'
-          >
-            <IconCashBanknote />
-            <div>
-              <div className='font-bold'>صندوق های سرمایه‌گذاری</div>
-              <div className=' text-sm text-gray-300'>
-                صندوق های سرمایه‌گذاری
-              </div>
-            </div>
-          </a>
-        </div>
       </div>
     </div>
   );
