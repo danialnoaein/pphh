3c931d91974149a2d4755364b98119c49d42fcbc
Amirhossein Mirsalari <mirsalari.dev@gmail.com>
2024-11-26 18:18:42 +0330

    fix issue in service card responsive

--- src/views/pages/home/BenefitsSection.tsx
+++ src/views/pages/home/BenefitsSection.tsx
@@ -3,14 +3,15 @@ import ServiceCard from "./serviceCard";
 
 const BenefitsSection = () => {
   return (
-    <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 px-8">
-      <div className="bg-white">
-        <div className="rounded-t-[2rem] md:rounded-t-[4rem] rounded-xl bg-white shadow-xl over overflow-hidden	">
+    <div className="container mx-auto grid lg:grid-cols-2 gap-8 px-8 w-full">
+      <div className="flex flex-col bg-white w-full">
+        <div className="flex-1 rounded-t-[2rem] md:rounded-t-[4rem] rounded-xl bg-white shadow-xl overflow-hidden w-full">
           <Image
             src={"/images/services-1.png"}
             alt={"section"}
             width={512}
-            height={512}
+            height={100}
+            className="object-cover h-[340px] w-full" 
           />
           <div className="p-8">
             <div className="mb-4">
@@ -28,9 +29,8 @@ const BenefitsSection = () => {
           </div>
         </div>
       </div>
-
-      <div className="bg-white">
-        <div className="rounded-t-[2rem] md:rounded-t-[4rem] rounded-xl bg-white shadow-xl over overflow-hidden	">
+      <div className="flex flex-col bg-white w-full">
+        <div className="flex-1 rounded-t-[2rem] md:rounded-t-[4rem] rounded-xl bg-white shadow-xl overflow-hidden w-full">
           <div>
             <ServiceCard />
           </div>
