6a6e976ab5bdd71a7deb5110e4a01ff6fc0d1b29
Amirhossein Mirsalari <mirsalari.dev@gmail.com>
2024-12-18 10:17:12 +0330

    fix style in navbar

--- src/views/pages/home/Hero.tsx
+++ src/views/pages/home/Hero.tsx
@@ -2,17 +2,17 @@
 
 const Hero = () => {
   return (
-    <div className='py-16 container mx-auto text-section text-center bg-center'>
+    <div className="py-16 container mx-auto text-section text-center bg-center">
       <br />
       <br />
       <br />
       <br />
-      <div className='hero'>
-        <div className='container mx-auto text-center font-bold text-primary-500 text-5xl'>
+      <div className="hero px-4">
+        <div className="container mx-auto text-center font-bold text-primary-500 text-4xl md:text-5xl">
           فردایی به اعتبار امروز
         </div>
 
-        <div className=' container mx-auto text-center text-black-500 text-2xl max-w-3xl mt-4'>
+        <div className=" container mx-auto text-center text-black-500 text-xl max-w-4xl mt-4">
           با تجربه‌ای ده‌ساله ما همراه شما هستیم تا با تحلیل‌های دقیق و خدمات
           اختصاصی، مسیر موفقیت شما را هموار کنیم.
         </div>
