3bea12557704c787415824b448d84629b06cc657
Amirhossein Mirsalari <mirsalari.dev@gmail.com>
2025-01-18 14:44:47 +0330

    feat: handle ports on production

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development",
+  },
+  serverRuntimeConfig: {
+    port: process.env.PORT || 3000,
+  },
+  publicRuntimeConfig: {
+    port: process.env.PORT || 3000,
+  },
 };
 
 export default nextConfig;
